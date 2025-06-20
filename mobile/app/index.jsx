43cf3fb0385b067ef30653e4e1c08fac295aa153
Matias Fernandez <matifer.dev@gmail.com>
2025-06-20 04:21:13 -0300

    Setting up styles & images

--- mobile/app/index.jsx
+++ mobile/app/index.jsx
@@ -1,18 +1,20 @@
 import { Image } from 'expo-image';
 import { Link } from 'expo-router';
-import { Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}
-    >
+    <View style={styles.container}>
       <Text>Edit app/index.tsx to edit this screen.</Text>
-      <Link href={'/about'}>About</Link>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'purple',
+  },
+});
