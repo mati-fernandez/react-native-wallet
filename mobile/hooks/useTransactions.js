4604a7358ff70bb60175d6b7050a51a929826fa4
Matias Fernandez <matifer.dev@gmail.com>
2025-06-28 20:45:46 -0300

    add: BalanceCard component

--- mobile/hooks/useTransactions.js
+++ mobile/hooks/useTransactions.js
@@ -2,7 +2,7 @@ import { useCallback, useState } from 'react';
 import { Alert } from 'react-native';
 
 // const API_URL = 'https://react-native-wallet-g7l1.onrender.com/api';
-const API_URL = 'http:192.168.68.104:5001/api';
+const API_URL = 'http:192.168.68.103:5001/api';
 // const API_URL = 'http:localhost:5001/api'; Este no sé por qué dejó de andar
 
 export const useTransactions = (userId) => {
